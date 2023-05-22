768355c380b5b687da103db92b8fe426cd007e34
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-22 15:21:14 -0500

    update: reorder installation commands

--- create_go_server.sh
+++ create_go_server.sh
@@ -12,14 +12,6 @@ project_name=${project_name:-my-go-server}
 read -p "Would you like to install Gin-Gonic web framework? (Y/n): " install_gin
 install_gin=${install_gin:-Y}
 
-# # Prompt for CompileDaemon installation
-# read -p "Would you like to install CompileDaemon package for live dev server update? (Y/n): " install_compile_daemon
-# install_compile_daemon=${install_compile_daemon:-Y}
-
-# # Prompt for godotenv installation
-# read -p "Would you like to install godotenv package? (Y/n): " install_godotenv
-# install_godotenv=${install_godotenv:-Y}
-
 # Prompt for database driver selection
 db_driver=""
 db_driver_option=""
@@ -68,46 +60,11 @@ git init
 echo "Done."
 echo
 
-# Create .gitignore
-echo "Creating sample .gitignore..."
-    cat << EOF > .gitignore
-# If you prefer the allow list template instead of the deny list, see community template:
-# https://github.com/github/gitignore/blob/main/community/Golang/Go.AllowList.gitignore
-#
-# Binaries for programs and plugins
-*.exe
-*.exe~
-*.dll
-*.so
-*.dylib
-
-# Test binary, built with `go test -c`
-*.test
-
-# Output of the go coverage tool, specifically when used with LiteIDE
-*.out
-
-# Dependency directories (remove the comment below to include it)
-# vendor/
-
-# Workspace file
-go.work
-.env
-$project_name
-EOF
-echo "Done."
-
 # Install CompileDaemon
 echo 
 echo "Running go get github.com/githubnemo/CompileDaemon && go install github.com/githubnemo/CompileDaemon..."
 go get github.com/githubnemo/CompileDaemon
 go install github.com/githubnemo/CompileDaemon
-echo "Done."
-
-# Install godotenv
-echo 
-echo "Running go get github.com/joho/godotenv..."
-go get github.com/joho/godotenv
 echo "Done."
 
 # Install Gin-Gonic if selected
@@ -215,6 +172,12 @@ case $db_driver_option in
             ;;
     esac
 
+# Install godotenv
+echo 
+echo "Running go get github.com/joho/godotenv..."
+go get github.com/joho/godotenv
+echo "Done."
+
 # Create .env
 echo 
 echo "Creating sample .env..."
@@ -235,7 +198,37 @@ $db_name=YOUR_DB_NAME
 EOF
 echo "Done."
 
-# Create Makefile
+
+# Add .gitignore
+echo "Creating sample .gitignore..."
+    cat << EOF > .gitignore
+# If you prefer the allow list template instead of the deny list, see community template:
+# https://github.com/github/gitignore/blob/main/community/Golang/Go.AllowList.gitignore
+#
+# Binaries for programs and plugins
+*.exe
+*.exe~
+*.dll
+*.so
+*.dylib
+
+# Test binary, built with `go test -c`
+*.test
+
+# Output of the go coverage tool, specifically when used with LiteIDE
+*.out
+
+# Dependency directories (remove the comment below to include it)
+# vendor/
+
+# Workspace file
+go.work
+.env
+$project_name
+EOF
+echo "Done."
+
+# Add Makefile
 echo 
 echo "Creating sample Makefile..."
 cat << EOF > Makefile
