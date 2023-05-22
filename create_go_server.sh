9e7615317bd6889b859ca1a922a4f2a012a46fe9
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-21 19:54:54 -0500

    feat: added project_name prompt

--- create_go_server.sh
+++ create_go_server.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+#############################################
+############     PROMPTS      ###############
+#############################################
+
+# Prompt for project name
+read -p "What is your project named? (Press Enter for default: my-go-server): " project_name
+project_name=${project_name:-my-go-server}
+#############################################
+#########    INSTALLATION      ##############
+#############################################
+
+# Generate and cd to the new directory
+mkdir $project_name
+cd $project_name
+
+# Run go mod init
+go mod init $project_name
