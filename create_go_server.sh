94202cbbdc0f845ee8d125fdca6b9029da466cd8
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-21 19:55:26 -0500

    feat: added install_gin prompt

--- create_go_server.sh
+++ create_go_server.sh
@@ -7,6 +7,10 @@
 # Prompt for project name
 read -p "What is your project named? (Press Enter for default: my-go-server): " project_name
 project_name=${project_name:-my-go-server}
+
+# Prompt for Gin-Gonic installation
+read -p "Would you like to install Gin-Gonic web framework? (Y/n): " install_gin
+install_gin=${install_gin:-Y}
 #############################################
 #########    INSTALLATION      ##############
 #############################################
@@ -17,3 +21,8 @@ cd $project_name
 
 # Run go mod init
 go mod init $project_name
+
+# Install Gin-Gonic if selected
+if [[ $install_gin =~ ^[Yy]$ ]]; then
+    go get -u github.com/gin-gonic/gin
+fi
