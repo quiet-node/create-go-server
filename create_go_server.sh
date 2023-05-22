fd4b4e3e58b3b4f494e4ee3d1b0f787681aab405
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-21 19:55:44 -0500

    feat: added install_compile_daemon prompt

--- create_go_server.sh
+++ create_go_server.sh
@@ -11,6 +11,10 @@ project_name=${project_name:-my-go-server}
 # Prompt for Gin-Gonic installation
 read -p "Would you like to install Gin-Gonic web framework? (Y/n): " install_gin
 install_gin=${install_gin:-Y}
+
+# Prompt for CompileDaemon installation
+read -p "Would you like to install CompileDaemon package for live dev server update? (Y/n): " install_compile_daemon
+install_compile_daemon=${install_compile_daemon:-Y}
 #############################################
 #########    INSTALLATION      ##############
 #############################################
@@ -25,4 +29,10 @@ go mod init $project_name
 # Install Gin-Gonic if selected
 if [[ $install_gin =~ ^[Yy]$ ]]; then
     go get -u github.com/gin-gonic/gin
+fi
+
+# Install CompileDaemon if selected
+if [[ $install_compile_daemon =~ ^[Yy]$ ]]; then
+    go get github.com/githubnemo/CompileDaemon
+    go install github.com/githubnemo/CompileDaemon
 fi
