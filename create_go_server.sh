be07e5916f9e8a0ae986d1265bcd5efffff193d7
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-21 19:56:27 -0500

    feat: added install_db_driver prompt

--- create_go_server.sh
+++ create_go_server.sh
@@ -19,6 +19,28 @@ install_compile_daemon=${install_compile_daemon:-Y}
 # Prompt for godotenv installation
 read -p "Would you like to install godotenv package? (Y/n): " install_godotenv
 install_godotenv=${install_godotenv:-Y}
+
+# Prompt for database driver selection
+db_driver=""
+db_driver_option=""
+while [[ ! $db_driver =~ ^(1|2|3)$ ]]; do
+    read -p "Which database driver would you like to install? (1) MongoDB, (2) PostgreSQL, (3) Others: " db_driver
+    case $db_driver in 
+        1)
+            db_driver_option="1"
+            ;;
+        2)
+            db_driver_option="2"
+            ;;
+        3)
+            db_driver_option="3"
+            ;;
+        *)
+            echo "Invalid option. Please select a valid database driver."
+            db_driver=""
+            ;;
+    esac
+done
 #############################################
 #########    INSTALLATION      ##############
 #############################################
@@ -45,3 +67,29 @@ fi
 if [[ $install_godotenv =~ ^[Yy]$ ]]; then
     go get github.com/joho/godotenv
 fi
+
+#Install dbdriver
+case $db_driver_option in
+        1)
+            go get go.mongodb.org/mongo-driver/mongo
+            env_file=".env"
+            db_uri="MONGODB_URI"
+            db_name="MONGO_DB"
+            ;;
+        2)
+            go get -u gorm.io/gorm
+            go get -u gorm.io/driver/postgres
+            env_file=".env"
+            db_uri="POSTGRESDB_URI"
+            db_name="POSTGRES_DB"
+            ;;
+        3)
+            env_file=""
+            echo "No database driver will be installed."
+            ;;
+        *)
+            echo "Invalid option. Please select a valid database driver."
+            db_driver=""
+            ;;
+    esac
+
